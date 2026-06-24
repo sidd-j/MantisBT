478c80784ba371409d1314a448f6d3964d5ed3d1
sidd-j <53267391+sidd-j@users.noreply.github.com>
2026-06-24 15:02:52 +0530

    Add MantisBT config

---
+++ config/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname = 'aws-1-ap-northeast-1.pooler.supabase.com:6543';
+$g_db_type = 'pgsql';
+$g_database_name = 'postgres';
+$g_db_username = 'postgres.wdndnocfbiixsfzjdxcy';
+$g_db_password = 'Siddhesh@1600';
+
+$g_default_timezone = 'UTC';
+
+$g_crypto_master_salt = 'a1vvLuWGCilppaViYjv/kIDKQFAVFKMm0im6bJVAPjI=';
+
+$g_path = 'https://mantisbt-ba47.onrender.com/';
