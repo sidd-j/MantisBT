12de1a219c0a81fba040b97602ddf1a0d29e19f6
sidd-j <53267391+sidd-j@users.noreply.github.com>
2026-06-24 17:29:49 +0530

    smtp settings

--- config/config_inc.php
+++ config/config_inc.php
@@ -10,6 +10,7 @@ $g_default_timezone = 'UTC';
 $g_crypto_master_salt = 'a1vvLuWGCilppaViYjv/kIDKQFAVFKMm0im6bJVAPjI=';
 
 $g_path = 'https://mantisbt-ba47.onrender.com/';
+$g_enable_email_notification = ON;
 
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'smtp.gmail.com';
@@ -17,3 +18,8 @@ $g_smtp_port = 465;
 $g_smtp_connection_mode = 'ssl';
 $g_smtp_username = 'jsiddhesh40@gmail.com';
 $g_smtp_password = 'kbzw rluv dhxm ohpl';
+
+$g_administrator_email = 'jsiddhesh40@gmail.com';
+$g_webmaster_email = 'jsiddhesh40@gmail.com';
+$g_from_email = 'jsiddhesh40@gmail.com';
+$g_return_path_email = 'jsiddhesh40@gmail.com';
